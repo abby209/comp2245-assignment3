c0533726535ef49b4b89e1a1da99270a1c387184
Abigail S <abiskepple@gmail.com>
2023-10-16 22:17:20 -0400

    tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,4 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
   gridSquares.forEach(function (square) {
     square.classList.add("square");
   });
+  
+  // Initialize the game state array (3x3 grid) with empty squares
+  const gameState = ["", "", "", "", "", "", "", "", ""];
+
+  // Select all squares in the grid using a CSS selector
+  const gridSquares = document.querySelectorAll(".square");
+
+  // Initialize a variable to keep track of the current player (X or O)
+  let currentPlayer = "X";
+
+  // Add a click event listener to each square
+  gridSquares.forEach(function (square, index) {
+    square.addEventListener("click", function () {
+      // Check if the square is empty (not already marked with X or O)
+      if (gameState[index] === "") {
+        // Update the game state array and the visual representation
+        gameState[index] = currentPlayer;
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+
+        // Toggle the current player (X to O or O to X)
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+      }
+    });
+  });
 });
