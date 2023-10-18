913a60f62379f2b9e52fe3911b076a36278d3316
Abigail S <abiskepple@gmail.com>
2023-10-17 20:40:33 -0400

    tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -79,3 +79,66 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const gameState = ["", "", "", "", "", "", "", "", ""];
+  const gridSquares = document.querySelectorAll(".square");
+  let currentPlayer = "X";
+  let gameWon = false;
+
+  // Function to check for a win
+  function checkForWin(player) {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const pattern of winPatterns) {
+      const [a, b, c] = pattern;
+      if (gameState[a] === player && gameState[b] === player && gameState[c] === player) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // Function to update the status message
+  function updateStatusMessage(message) {
+    const status = document.getElementById("status");
+    status.textContent = message;
+    status.classList.add("you-won");
+  }
+
+  gridSquares.forEach(function (square, index) {
+    square.addEventListener("click", function () {
+      if (gameState[index] === "" && !gameWon) {
+        gameState[index] = currentPlayer;
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+
+        if (checkForWin(currentPlayer)) {
+          gameWon = true;
+          updateStatusMessage(`Congratulations! ${currentPlayer} is the Winner!`);
+        } else {
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
+      }
+    });
+
+    square.addEventListener("mouseover", function () {
+      if (gameState[index] === "" && !gameWon) {
+        square.classList.add("hover");
+      }
+    });
+
+    square.addEventListener("mouseout", function () {
+      square.classList.remove("hover");
+    });
+  });
+});
